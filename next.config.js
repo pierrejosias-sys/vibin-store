58533b0cf7323358d758bd46d9258ec9656f86e3
Josias L Pierre <Pierrejosias@icloud.com>
2026-04-29 18:55:09 -0400

    Fix next.config.js syntax

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const nextConfig = {
       'images.unsplash.com',
       'grbwnjnngzcsjlubcmtp.supabase.co'
     ],
-  },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://grbwnjnngzcsjlubcmtp.supabase.co',
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdyYnduam5uZ3pjc2psdWJjbXRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNTgzMDEsImV4cCI6MjA5MjkzNDMwMX0.SYKFZJ0XVua0JmZ-tkaNhec2M3KtG3tS5vj_1Nl261c'
   }
 }
 
