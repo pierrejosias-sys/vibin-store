e4caff6f77b0182923d2d45e013be60c7b557060
pierrejosias-sys <pierrejosias@icloud.com>
2026-06-16 15:43:00 -0400

    fix: allow S3 and external image domains in Next.js config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-  reactStrictMode: true,
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'user-gen-media-assets.s3.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.in',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
