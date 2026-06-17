e85b3e31217b95cf4a9920195410c309529ecc90
pierrejosias-sys <pierrejosias@icloud.com>
2026-06-16 21:40:47 -0400

    fix: remove duplicate root layout.js (app/layout.js is the real one)

--- layout.js
+++
@@ -1,3 +0,0 @@
-import "./globals.css"
-import Link from "next/link"
-export default function Layout({children}){return(<html><body className="bg-black text-white min-h-screen">{children}</body></html>)}
